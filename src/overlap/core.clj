77a0aef3c673012a1cb45c2a6cc92899fd565267
CarnunMP <carnun@hotmail.co.uk>
2021-11-11 22:57:40 +0000

    Switch to :fill mode on close of shape

--- src/overlap/core.clj
+++ src/overlap/core.clj
@@ -5,30 +5,47 @@
 ; [x] draw points on click
 ; [x] reset on :r keypress
 ; [x] draw lines between sucessive points
-; [] when click near initial point, switch into 'fill' (?) mode
+; [x] when click near initial point, close the shape and switch into 'fill' (?) mode
 ; [] pixel-by-pixel, colour according to whether 'internal' or 'external' to shape
 ; ...
 
+
+(def init-state {:points [] ; vector of [x y] points
+                 :mode :draw ; options are :draw and :fill
+                 })
+(def p1-radius 20)
+
+
 ;;; helpers
+
 (defn- get-line-points [points]
   (map #(vector %1 %2) points (rest points)))
 
-;;; quil functions
+(defn- click-near-p1? [x y [x1 y1]]
+  (when x1
+    (let [[xd yd] [(- x x1) (- y y1)]]
+      (>= p1-radius (Math/sqrt (+ (* xd xd)
+                                  (* yd yd)))))))
 
-(def init-state {:points []})
+;;; quil functions
 
 (defn setup []
   (q/frame-rate 30)
   (q/stroke 255)
+  (q/fill nil)
 
   init-state)
 
 (defn update-state [state]
   state)
 
-(defn mouse-pressed [state {:keys [x y button]}]
+(defn mouse-pressed [{:keys [points mode] :as state} {:keys [x y button]}]
   (if (= :left button)
-    (update state :points conj [x y])
+    (if (click-near-p1? x y (first points))
+      (assoc state :mode :fill)
+      (if (= :draw mode)
+        (update state :points conj [x y])
+        state))
     state))
 
 (defn key-pressed [state {:keys [key]}]
@@ -36,14 +53,21 @@
     init-state
     state))
 
-(defn draw-state [{:keys [points] :as state}]
+(defn draw-state [{:keys [points mode] :as state}]
   (q/background 0)
 
+  ;; draw points
   (doseq [[x y] points]
     (q/ellipse x y 2 2))
-
+  ;; draw circle around p1
+  (when-let [[x1 y1] (first points)]
+    (q/ellipse x1 y1 (* p1-radius 2) (* p1-radius 2)))
+  ;; draw lines
   (doseq [[p1 p2] (get-line-points points)]
     (q/line p1 p2))
+  ;; draw final line
+  (when (= :fill mode)
+    (q/line (peek points) (first points)))
   )
 
 (q/defsketch #_:clj-kondo/ignore overlap
